81a54b7a7e55276ac2651d15f5b6e767c47d4c67
Samuel Ling <samuel.ling977@gmail.com>
2021-05-14 11:56:57 +0800

    implements movile's property and and use them in the movie list, also added the modx

--- src/components/pages/Home/MovieList/Movie.tsx
+++ src/components/pages/Home/MovieList/Movie.tsx
@@ -1,42 +1,48 @@
+import { inject, observer } from 'mobx-react';
 import React from 'react';
 import {
     Text,
     View,
     StyleSheet,
-    FlatList,
+    TouchableHighlight,
     Button,
     Image
 } from 'react-native';
 import tailwind from 'tailwind-rn';
 import { Card } from '../../../UI/Card';
 import { ChipList } from "./ChipList/ChipList";
-export const Movie = () => {
+export const Movie = (props : any) => {
+    const clickedHandler = () => {
+        props.onClick(props.id)
+    }
+    const characters = props.characters.map((CharacterData : any) => CharacterData.Character.name)
+
     return (
+        <TouchableHighlight underlayColor="#f0f4f7" onPress={() => console.log("clicked")}>
         <Card>
         <View style={tailwind("flex flex-row overflow-hidden")}>
             <Image
-                style={
-                    [{ width: 124, height: 186, resizeMode: "contain" },
-                    tailwind("")
-                    ]
+                style={ [{ width: 94, height: 166, resizeMode: "contain" },
+                    tailwind("bg-gray-400")
+                ]
                 }
-                source={{ uri: "https://picsum.photos/200/300" }}
+                source={{ uri: props.image }}
             />
             <View style={tailwind("ml-3")}>
-                <Text style={tailwind("text-2xl")}>Iron Man : 2</Text>
+                <Text style={tailwind("text-2xl")}>{props.name}</Text>
                 <Text style={tailwind("w-60")}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quae repudiandae dolor neque id ratione modi libero ab exercitationem.
-                    Ratione ab quas dolorem vero officiis aperiam nesciunt beatae quae dicta.
+                   {props.description}
                 </Text>
                 <View
                     style={tailwind("border-b border-gray-100 my-2")}
                 />
                 <View>
                     <Text style={tailwind("mb-1")}>Characters</Text>
-                    <ChipList items={["Luke Skywalker", "C-3PO", "Darth Vader"]}></ChipList>
+                    <ChipList items={characters || ["No Charactersr"]}></ChipList>
                 </View>
             </View>
         </View>
         </Card>
+        </TouchableHighlight>
     )
 }
--- src/components/pages/Home/MovieList/MovieList.tsx
+++ src/components/pages/Home/MovieList/MovieList.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react"
+import { useQuery } from "@apollo/client";
+import { inject, observer } from "mobx-react";
+import React, { useContext, useState } from "react"
 import {
     Text,
     View,
@@ -7,15 +9,31 @@ import {
     Button,
     Image
 } from 'react-native';
+import { useLocation } from "react-router";
 import tailwind from 'tailwind-rn';
+import { FETCH_MOVIES } from "../../../../helper/query";
+import { MovieStoreContext } from "../../../../store/MovieStore/MovieStore";
 import { ChipList } from "./ChipList/ChipList";
 import { Movie } from "./Movie";
-export const MovieList = () => {
+
+export const MovieList = inject("store")(observer((props : any) => {
+    const location = useLocation();
+    const { loading, error, data } = useQuery(FETCH_MOVIES)
+    console.log({ loading, error, data });
+    if (loading == false && !error) {
+        props.store.movieStore.loadMovies(data.movie)
+    }
+
+    console.log(props.store.movieStore)
+
+       
     return (
         <View style={tailwind("flex-1")}>
             <ScrollView style={tailwind("h-full mb-20")} >
-                {[1,2,3].map(e => <Movie></Movie>)}
+                {props.store.movieStore.movies.map((data : any) => (
+                    <Movie id={data.id} name={data.name} description={data.description} characters={data.MovieCharacter}></Movie>
+                ))}
             </ScrollView >
         </View>
     )
-}
+}))
