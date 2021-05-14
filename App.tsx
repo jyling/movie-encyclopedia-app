c674643c686838b4a1bdd793643fd233518709d4
Samuel Ling <samuel.ling977@gmail.com>
2021-05-14 11:53:47 +0800

    added apolloprovider and store provider to the App Component

--- App.tsx
+++ App.tsx
@@ -5,34 +5,37 @@
  * @format
  * @flow strict-local
  */
-import { NativeRouter, Route, Link } from "react-router-native";
-import React from 'react';
+import { NativeRouter } from "react-router-native";
+import React, { useContext } from 'react';
 import type { Node } from 'react';
 import {
-  Text,
-  View,
   StyleSheet,
 } from 'react-native';
-import tailwind from 'tailwind-rn';
-import { Navigation } from './src/components/UI/Navigation/Navigation';
-import { Home } from "./src/components/pages/Home/Home";
-import { Create } from "./src/components/pages/Create/Create";
+import { observer, Provider } from "mobx-react"
+import { PageWrapper } from "./src/components/UI/PageWrapper";
+import { MovieStoreContext } from "./src/store/MovieStore/MovieStore";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const App: () => Node = () => {
+const client = new ApolloClient({
+  uri: 'http://192.168.1.150:3000/graphql',
+  cache: new InMemoryCache()
+});
 
+const App: () => Node = observer(() => {
+  const movies = useContext(MovieStoreContext);
   return (
-    <NativeRouter>
-      <View style={tailwind("min-w-full bg-white")}>
-        <Navigation></Navigation>
-        <View style={tailwind("min-h-full bg-gray-500")}>
-          <Route exact path="/" component={Home} />
-          <Route path="/create" component={Create} />
-        </View>
-      </View>
-    </NativeRouter>
+    <ApolloProvider client={client}>
+      <Provider store={{ 
+        movieStore : movies
+       }}>
+        <NativeRouter>
+          <PageWrapper></PageWrapper>
+        </NativeRouter>
+      </Provider>
+    </ApolloProvider>
   )
-};
+});
 
 const style = StyleSheet.create({
   NavigationContainer: {
