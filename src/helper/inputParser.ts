cf73c3736675e04edd7ae7f635976e9cb5ce1774
Samuel Ling <samuel.ling977@gmail.com>
2021-05-14 16:54:32 +0800

    implements custom input parser

---
+++ src/helper/inputParser.ts
@@ -0,0 +1,33 @@
+
+interface TermObject {
+    [name :string] : string
+}
+
+export const inputParser = (searchTerm: any) => {
+    let searchTermArray = searchTerm.split(" ")
+    searchTermArray = searchTermArray.map((word: string) => {
+        if (word.indexOf(":") != -1) {
+            let obj : TermObject = {}
+            obj[word.split(":")[0].toLowerCase()] = word.split(":")[1].split("_").join(" ")
+            return obj
+        }
+        return word
+    })
+    let searchTermName = searchTermArray.filter((term : any) => {
+        return typeof term == "string"
+    }).join(" ")
+
+    searchTermArray = searchTermArray.filter((term : any) => {
+        return typeof term != "string"
+    });
+
+    if (searchTermName) searchTermArray.push({ name: searchTermName })
+    searchTermArray = searchTermArray.filter((term : any) => {
+        return ["id","name","releasedDate,description","characters","genres","directors","writers"].indexOf(Object.keys(term)[0]) != -1
+    })
+    var returnObj = {}
+    searchTermArray.forEach((term : any) => {
+        returnObj = {...returnObj, ...term}
+    });
+    return returnObj;
+}
