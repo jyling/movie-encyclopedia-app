45d537053c54a846a6e075ac64f30285e5b7f4d3
Samuel Ling <samuel.ling977@gmail.com>
2021-05-14 16:56:36 +0800

    added emit for the search component

--- src/components/pages/Home/Search/Search.tsx
+++ src/components/pages/Home/Search/Search.tsx
@@ -10,16 +10,21 @@ import {
 import tailwind from 'tailwind-rn';
 import { Card } from '../../../UI/Card';
 import {InputWithButton} from '../../../UI/InputWithButton'
-export const Search = () => {
+export const Search = (props : any) => {
 
     const [searchTerm, setSearchTerm] = useState(null);
     const searchTermHandler = (e :any) => {
         setSearchTerm(e)
+        props.onSearchTermChange(e)
+    }
+    const searchButtonHandler = (e : any) => {
+        props.onButtonClick(e)
     }
     return (
         <Card>
         <InputWithButton 
             onContentChange={searchTermHandler}
+            onButtonClick={searchButtonHandler}
             value={searchTerm}
             button="Search"
         ></InputWithButton>
