07a6e76798352f0cd31b9abf378c310ef6b4a2ba
Samuel Ling <samuel.ling977@gmail.com>
2021-05-14 16:57:17 +0800

    fixed a typo

--- src/components/UI/InputWithButton.tsx
+++ src/components/UI/InputWithButton.tsx
@@ -5,36 +5,42 @@ import {
     StyleSheet,
     TextInput,
     Button,
-    Alert
+    Alert,
+    TouchableWithoutFeedback,
+    Keyboard
 } from 'react-native';
 import tailwind from 'tailwind-rn';
 
-export function InputWithButton(props) {
+export function InputWithButton(props : any) {
     const [content, setContent] = useState(props.value)
-    const contentHandler= (value) =>{
+    const contentHandler = (value : any) => {
         setContent(value)
         props.onContentChange(value);
     }
 
-    const buttonClickHandler= (value) =>{
-        props.onButtonClicked();
+    const buttonClickHandler = (value : any) => {
+        console.log("clicked from input with button")
+        props.onButtonClick();
     }
     return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View
             style={tailwind("flex flex-row justify-between items-end mb-2")}
         >
-
-            <TextInput
-                style={tailwind("flex-1 border-b-2 border-gray-300 text-black")}
-                onChangeText={contentHandler}
-                value={content}
-                placeholder={props.placeholder}
-            />
+                <View  style={{ flex: 1 }}>
+                <TextInput
+                    style={tailwind("flex-1 border-b-2 border-gray-300 text-black")}
+                    onChangeText={contentHandler}
+                    value={content}
+                    placeholder={props.placeholder}
+                />
+                </View>
             <View style={tailwind("pl-1")}>
                 <Button
-                onTap={buttonClickHandler}
-                title={props.button}></Button>
-                </View>
+                    onPress={buttonClickHandler}
+                    title={props.button}></Button>
+            </View>
         </View>
+                    </TouchableWithoutFeedback>
     )
 }
