7442477fc68cdd3b5361e2fa331ebaa3ed03e49e
Samuel Ling <samuel.ling977@gmail.com>
2021-05-16 19:32:21 +0800

    added alert box

--- src/components/pages/MovieView/MovieView.tsx
+++ src/components/pages/MovieView/MovieView.tsx
@@ -2,7 +2,7 @@ import { useLazyQuery, useQuery } from '@apollo/client';
 import { inject } from 'mobx-react';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { useHistory } from 'react-router';
 import tailwind from 'tailwind-rn';
@@ -13,55 +13,71 @@ import { ChipList } from '../../UI/ChipList/ChipList';
 import { Divider } from '../../UI/Divider';
 import { ChipListMovie } from './ChipListMovie';
 
-export const MovieView = inject("store")((props : any) => {
-    let { match : {params: {id}}} = props
+export const MovieView = inject("store")((props: any) => {
+    let { match: { params: { id } } } = props
     const history = useHistory();
-    const movie = props.store.movieStore.movies.find((movie : any) => movie.id == id)
-    const characters= movie.MovieCharacter.map((characterData : any) => characterData.Character.name)
-    const genres = movie.MovieGenre.map((genreData : any) => genreData.Genre.name)
-    const writers = movie.MovieWriter.map((writerData : any) => writerData.People.name)
-    const directors = movie.MovieDirector.map((directorData : any) => directorData.People.name)
+    const movie = props.store.movieStore.movies.find((movie: any) => movie.id == id)
+    const characters = movie.MovieCharacter.map((characterData: any) => characterData.Character.name)
+    const genres = movie.MovieGenre.map((genreData: any) => genreData.Genre.name)
+    const writers = movie.MovieWriter.map((writerData: any) => writerData.People.name)
+    const directors = movie.MovieDirector.map((directorData: any) => directorData.People.name)
     const formatedDate = moment(movie.releasedDate).format("MMMM Do YYYY");
     console.log(movie)
     const editHandler = () => {
         history.push(`/edit/movie/${movie.id}`)
     }
 
+    const deleteHandler = () => {
+        Alert.alert(
+            'Alert Title',
+            'Alert message here...',
+            [
+              {text: 'NO', onPress: () => console.warn('NO Pressed'), style: 'cancel'},
+              {text: 'YES', onPress: () => console.warn('YES Pressed')},
+            ]
+        );
+    }
+
     return (
         <ScrollView style={tailwind("mb-20")}>
             <View style={tailwind("p-2")}>
-            <View style={tailwind("flex flex-row justify-between items-end px-3 mb-3")}>
-                <BackButton></BackButton>
+                <View style={tailwind("flex flex-row justify-between items-end px-3 mb-3")}>
+                    <BackButton></BackButton>
+                </View>
+                <Card>
+                    <View style={tailwind("flex justify-center items-end mb-3")}>
+                        <View style={tailwind("flex flex-row")}>
+                            <Button onPress={editHandler} title="Edit" buttonStyle={
+                                tailwind("bg-yellow-500 px-5 mr-2")
+                            }></Button>
+                            <Button onPress={deleteHandler} title="Delete" buttonStyle={
+                                tailwind("bg-red-500 px-5")
+                            }></Button>
+                        </View>
+                    </View>
+                    <View style={tailwind("flex justify-center items-center mb-3")}>
+                        <Image
+                            style={[{ width: 154, height: 236, resizeMode: "cover" },
+                            tailwind("bg-gray-400")
+                            ]
+                            }
+                            source={{ uri: movie.imageURL || null }}
+                        />
+                    </View>
+                    <Text style={tailwind("text-3xl")}>{movie.name}</Text>
+                    <Divider></Divider>
+                    <Text>Released on {formatedDate}</Text>
+                    <Divider></Divider>
+                    <Text>Description :</Text>
+                    <Text>{movie.description}</Text>
+                    <Divider></Divider>
+                    <ChipListMovie name={"Genre"} items={genres}></ChipListMovie>
+                    <ChipListMovie name={"Character"} items={characters}></ChipListMovie>
+                    <ChipListMovie name={"Writer"} items={writers}></ChipListMovie>
+                    <ChipListMovie name={"Director"} items={directors}></ChipListMovie>
+
+                </Card>
             </View>
-            <Card>
-            <View style={tailwind("flex justify-center items-end mb-3")}>
-                <Button onPress={editHandler} title="Edit" buttonStyle={
-                    tailwind("bg-yellow-500 px-5")
-                }></Button>
-            </View>
-            <View style={tailwind("flex justify-center items-center mb-3")}>
-            <Image
-                style={ [{ width: 154, height: 236, resizeMode: "cover" },
-                    tailwind("bg-gray-400")
-                ]
-                }
-                source={{ uri: movie.imageURL || null }}
-            />
-            </View>
-                <Text style={tailwind("text-3xl")}>{movie.name}</Text>
-                <Divider></Divider>
-                <Text>Released on {formatedDate}</Text>
-                <Divider></Divider>
-                <Text>Description :</Text>
-                <Text>{movie.description}</Text>
-                <Divider></Divider>
-                <ChipListMovie name={"Genre"} items={genres}></ChipListMovie>
-                <ChipListMovie name={"Character"} items={characters}></ChipListMovie>
-                <ChipListMovie name={"Writer"} items={writers}></ChipListMovie>
-                <ChipListMovie name={"Director"} items={directors}></ChipListMovie>
-                
-            </Card>
-        </View>
         </ScrollView>
     )
 })
