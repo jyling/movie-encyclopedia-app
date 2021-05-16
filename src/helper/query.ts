c6b190777e67065100bb0ee04e4ce32795847d73
Samuel Ling <samuel.ling977@gmail.com>
2021-05-16 10:11:20 +0800

    implement FETCH_CHARACTER, FETCH_GENRE, FETCH_PEOPLE and FETCH_CHARACTER_PEOPLE_GENRE,

--- src/helper/query.ts
+++ src/helper/query.ts
@@ -38,6 +38,97 @@ query {
 
 `
 
+export const FETCH_CHARACTER = gql`
+query {
+  character {
+    id
+    name,
+    MovieCharacter {
+      Movie {
+        id
+        name
+      }
+    }
+  }
+}
+
+`
+
+export const FETCH_GENRE = gql`
+query {
+  genre {
+    id
+    name,
+     MovieGenre {
+      Movie {
+        id
+        name
+      }
+    }
+  }
+}
+
+`
+
+export const FETCH_PEOPLE = gql`
+query{
+  people {
+    id
+    name
+    MovieDirector {
+      People {
+        name
+      }
+    }
+      MovieWriter {
+        People {
+          name
+        }
+      }
+  }
+}
+
+`
+
+export const FETCH_CHARACTER_PEOPLE_GENRE = gql`
+query {
+  people {
+    id
+    name
+    MovieDirector {
+      People {
+        name
+      }
+    }
+      MovieWriter {
+        People {
+          name
+        }
+      }
+  }
+  genre {
+    id
+    name,
+     MovieGenre {
+      Movie {
+        id
+        name
+      }
+    }
+  }
+  character {
+    id
+    name,
+    MovieCharacter {
+      Movie {
+        id
+        name
+      }
+    }
+  }
+}
+`
+
 export const SEARCH_MOVIES = () => {
   return gql`
 query getMovies($searchMovieInput : SearchMovieInput, $limit: Int, $page: Int){
@@ -83,3 +174,65 @@ query getMovies($searchMovieInput : SearchMovieInput, $limit: Int, $page: Int){
 
 `
 }
+
+export const INSERT_MOVIE = gql`
+mutation createMovie($createMovieInput : CreateMovieInput){
+  createMovie(
+    createMovieInput: $createMovieInput
+  ) { 
+      id
+      name
+      description
+      releasedDate
+      
+      MovieGenre {
+        Genre {
+          name
+          description
+        }
+      }
+      
+      MovieCharacter {
+        Character {
+          name
+        }
+      }
+      
+      MovieWriter {
+        People {
+          name
+        }
+      }
+      
+      MovieDirector {
+        People {
+          name
+        }
+      }
+    }
+}
+`
+
+export const FIND_MOVIE = gql`
+query movie_find($id : Int!) {
+  movie_find(id: $id) {
+    name
+    description
+    MovieCharacter {
+      Character {
+        name
+      }
+    }
+    MovieGenre {
+      Genre {
+        name
+      }
+    }
+    MovieWriter {
+      People {
+        name
+      }
+    }
+  }
+}
+`
