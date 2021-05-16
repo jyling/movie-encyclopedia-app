56eb4010c6764de5e0111ef08455e4406de87b00
Samuel Ling <samuel.ling977@gmail.com>
2021-05-16 10:09:24 +0800

    button use button from react native elements

--- src/components/UI/InputWithButton.tsx
+++ src/components/UI/InputWithButton.tsx
@@ -1,26 +1,33 @@
+import { serializeFetchParameter } from "@apollo/client";
 import React, { useState } from "react"
 import {
     Text,
     View,
     StyleSheet,
     TextInput,
-    Button,
     Alert,
     TouchableWithoutFeedback,
     Keyboard
 } from 'react-native';
+import { Button } from "react-native-elements/dist/buttons/Button";
 import tailwind from 'tailwind-rn';
 
 export function InputWithButton(props : any) {
     const [content, setContent] = useState(props.value)
     const contentHandler = (value : any) => {
         setContent(value)
-        props.onContentChange(value);
+        if (props.onContentChange) {
+            props.onContentChange(value);
+        }
     }
 
     const buttonClickHandler = (value : any) => {
-        console.log("clicked from input with button")
-        props.onButtonClick();
+        if (props.onButtonClick) {
+            props.onButtonClick(content);
+        }
+        if (props.deleteContentOnButtonClick) {
+            setContent("")
+        }
     }
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
@@ -36,9 +43,7 @@ export function InputWithButton(props : any) {
                 />
                 </View>
             <View style={tailwind("pl-1")}>
-                <Button
-                    onPress={buttonClickHandler}
-                    title={props.button}></Button>
+                <Button onPress={buttonClickHandler} buttonStyle={tailwind("bg-blue-400")} title={props.button}></Button>
             </View>
         </View>
                     </TouchableWithoutFeedback>
