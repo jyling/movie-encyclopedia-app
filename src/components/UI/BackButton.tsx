1fbec401f082b175d606e73aaf0d85913079bca6
Samuel Ling <samuel.ling977@gmail.com>
2021-05-16 10:34:21 +0800

    implements button from react native elements

--- src/components/UI/BackButton.tsx
+++ src/components/UI/BackButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { Button } from 'react-native-elements';
 import { useHistory } from 'react-router';
+import tailwind from 'tailwind-rn';
 
 export const BackButton = (props : any) => {
     const history = useHistory();
@@ -11,5 +12,5 @@ export const BackButton = (props : any) => {
             history.push(props.path)
         }
     }
-    return (<Button style={props.style} color={props.color} onPress={backButtonClicked} title={props.title || "Back"}></Button>)
+    return (<Button style={[props.style, tailwind("bg-blue-500 px-5")]} onPress={backButtonClicked} title={props.title || "Back"}></Button>)
 }
