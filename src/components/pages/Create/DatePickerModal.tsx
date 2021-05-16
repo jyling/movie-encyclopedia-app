b4ef5e93763cca2fbdf460b43e928ae607b9df6d
Samuel Ling <samuel.ling977@gmail.com>
2021-05-16 10:13:50 +0800

    abstrat the date picker modal function

---
+++ src/components/pages/Create/DatePickerModal.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Button } from "react-native-elements";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+export const DatePickerModal = (props) => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    props.onDateSelected(date);
+    hideDatePicker();
+  };
+
+  return (
+    <View>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+    </View>
+  );
+};
