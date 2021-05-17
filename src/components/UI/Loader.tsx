b4be15c44a111cbd5f2e4be7e956103e53ff01d6
Samuel Ling <samuel.ling977@gmail.com>
2021-05-17 15:23:58 +0800

    made the default value to false

--- src/components/UI/Loader.tsx
+++ src/components/UI/Loader.tsx
@@ -5,12 +5,12 @@ import tailwind from 'tailwind-rn';
 
 
 export const Loader = (props) => {
-
+    console.log({loader: props})
     return (
         <Modal
             animationType="fade"
             transparent={true}
-            visible={props.loading}
+            visible={props.loading || false}
         >
             <View
                 style={tailwind("flex-1 justify-center items-center p-3 bg-black bg-opacity-50")}
