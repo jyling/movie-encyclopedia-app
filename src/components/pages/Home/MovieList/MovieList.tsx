a7cdd9df076d2d6e5f2196794a5b67701a38b4fd
Samuel Ling <samuel.ling977@gmail.com>
2021-05-17 15:27:26 +0800

    added default item for empty movie list

--- src/components/pages/Home/MovieList/MovieList.tsx
+++ src/components/pages/Home/MovieList/MovieList.tsx
@@ -4,7 +4,7 @@ import {
     Text,
     View,
     StyleSheet,
-    ScrollView ,
+    ScrollView,
     Button,
     Image
 } from 'react-native';
@@ -13,10 +13,9 @@ import tailwind from 'tailwind-rn';
 import { Card } from "../../../UI/Card";
 import { Movie } from "./Movie";
 
-export const MovieList = inject("store")((props : any) => {
+export const MovieList = inject("store")((props: any) => {
 
     let currentPage = props.page;
-    console.log({currentPage})
     const pagePrevUpdateHandler = () => {
         if ((currentPage - 1) <= 0) {
             props.onPageChanged(currentPage)
@@ -32,21 +31,30 @@ export const MovieList = inject("store")((props : any) => {
         }
         props.onPageChanged(++currentPage)
     }
-
+    const containMovies = (props.movies.length == 0) ? false : true;
     return (
         <View style={tailwind("flex-1")}>
-            <ScrollView style={tailwind("h-80 mb-20")} >
-                {props.movies.map((data : any) => (
-                    <View  key={data.id}>
-                        <Movie id={data.id} name={data.name} image={data.imageURL || null} description={data.description} characters={data.MovieCharacter}></Movie>
-                    </View>
-                ))}
-            <Card style={tailwind("flex-1 flex-row justify-between items-center")}>
-                <Button disabled={((currentPage - 1) <= 0)} onPress={pagePrevUpdateHandler} title="Previous Page" ></Button>
-                <Text>{props.page || 1} out of {props.totalPage || 1} pages</Text>
-                <Button disabled={((currentPage + 1) > props.totalPage)} onPress={pageNextUpdateHandler} title="Next Page"> </Button>
-            </Card>
-            </ScrollView >
+            {!containMovies &&
+                <Card>
+                    <Text style={tailwind("text-center text-black text-3xl")}>No Result</Text>
+                    <Text style={tailwind("text-center text-black text-sm")}>We couldn't find any movies</Text>
+                </Card>
+            }
+            {containMovies &&
+                <ScrollView style={tailwind("h-80 mb-20")} >
+                    {props.movies.map((data: any) => (
+                        <View key={data.id}>
+                            <Movie id={data.id} name={data.name} image={data.imageURL || null} description={data.description} characters={data.MovieCharacter}></Movie>
+                        </View>
+                    ))}
+
+                    <Card style={tailwind("flex-1 flex-row justify-between items-center")}>
+                        <Button disabled={((currentPage - 1) <= 0)} onPress={pagePrevUpdateHandler} title="Previous Page" ></Button>
+                        <Text>{props.page || 1} out of {props.totalPage || 1} pages</Text>
+                        <Button disabled={((currentPage + 1) > props.totalPage)} onPress={pageNextUpdateHandler} title="Next Page"> </Button>
+                    </Card>
+                </ScrollView >
+            }
         </View>
     )
 })
