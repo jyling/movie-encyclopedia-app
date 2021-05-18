0ac5914945213487f292e0f034ee037438024e2a
Samuel Ling <samuel.ling977@gmail.com>
2021-05-18 15:25:23 +0800

    added comment explaination on how does the search algorithm work

--- src/helper/inputParser.ts
+++ src/helper/inputParser.ts
@@ -2,9 +2,10 @@
 interface TermObject {
     [name :string] : string
 }
-
+//searchTerm = characters:tony_stark test
 export const inputParser = (searchTerm: any) => {
     let searchTermArray = searchTerm.split(" ")
+    // [ characters:tony_stark, test]
     searchTermArray = searchTermArray.map((word: string) => {
         if (word.indexOf(":") != -1) {
             let obj : TermObject = {}
@@ -13,15 +14,20 @@ export const inputParser = (searchTerm: any) => {
         }
         return word
     })
+    // [ { characters: "tony_stark" }, "test"]
     let searchTermName = searchTermArray.filter((term : any) => {
         return typeof term == "string"
     }).join(" ")
+    // [ "test"]
 
     searchTermArray = searchTermArray.filter((term : any) => {
         return typeof term != "string"
     });
+    // [ { characters: "tony_stark" }]
 
     if (searchTermName) searchTermArray.push({ name: searchTermName })
+    // [ { characters: "tony_stark" }, {name: "test"}]
+
     searchTermArray = searchTermArray.filter((term : any) => {
         return ["id","name","releasedDate","description","characters","genres","directors","writers"].indexOf(Object.keys(term)[0]) != -1
     })
